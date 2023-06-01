cc7500241270b843b27e43da996bebae50a75154
ug-zwq <114631560+z112734575@users.noreply.github.com>
2023-06-01 16:26:07 +0800

    修改打爆配置

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
+console.log(process.env.NODE_ENV === 'production' , 'rinima')
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? './': '/',
   resolve: {
+    publicDir:  process.env.NODE_ENV === 'production' ? './': '/',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
